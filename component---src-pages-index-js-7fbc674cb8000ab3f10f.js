6920df99238104b30b723f3211efd88002d9bbaa
frasemcl <fraser.mclaughlin@outlook.com>
2023-10-07 10:12:03 -0400

    minor changes

--- component---src-pages-index-js-7fbc674cb8000ab3f10f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,a){a.r(t),a.d(t,{Head:function(){return l}});var n=a(7294);const l=()=>n.createElement("title",null,"Home Page");t.default=()=>n.createElement("main",null,n.createElement("h1",null,"Nothing to see here"))}}]);
-//# sourceMappingURL=component---src-pages-index-js-7fbc674cb8000ab3f10f.js.map
--- component---src-pages-using-typescript-tsx-a858fad4485c40ebb0a5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[970],{8619:function(){}}]);
